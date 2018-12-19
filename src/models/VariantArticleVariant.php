b7458fd669f670854b39a974a71144217e37428d
Georg Nagel <g.schlmm@gmail.com>
2018-12-19 14:33:53 +0100

    add variantArticle, variantArticleAttribute & variantArticleVariant

---
+++ src/models/VariantArticleVariant.php
@@ -0,0 +1,13 @@
+<?php namespace Geccomedia\Weclapp\Models;
+
+use Geccomedia\Weclapp\WeclappModel;
+
+class VariantArticleVariant extends WeclappModel
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'variantArticleVariant';
+}
